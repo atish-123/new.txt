a31d1a90894da2cc35542bf202032a0fe3c4af75
atish-123 <114129195+atish-123@users.noreply.github.com>
2022-09-29 16:30:30 +0530

    Add files via upload

--- ecommerce.sql
+++ ecommerce.sql
@@ -93,3 +93,40 @@ CREATE TABLE `ecommerce`.`orderdetails` (
     REFERENCES `ecommerce`.`products` (`productid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+	
+------------------------------------Deliveries--------------------------
+
+CREATE TABLE `ecommerce`.`deleveries` (
+  `deleiveryid` INT NOT NULL,
+  `status` VARCHAR(200) NOT NULL,
+  `orderid` INT NOT NULL,
+  `shipperid` INT NOT NULL,
+  PRIMARY KEY (`deleiveryid`),
+  INDEX `deliveryorders_idx` (`orderid` ASC) VISIBLE,
+  INDEX `deliveryshippers_idx` (`shipperid` ASC) VISIBLE,
+  CONSTRAINT `deliveryorders`
+    FOREIGN KEY (`orderid`)
+    REFERENCES `ecommerce`.`orders` (`orderid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `deliveryshippers`
+    FOREIGN KEY (`shipperid`)
+    REFERENCES `ecommerce`.`shippers` (`shipperid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
+-------------------------------Payments----------------------
+CREATE TABLE `ecommerce`.`payments` (
+  `paymentid` INT NOT NULL,
+  `paymentdate DATETIME` VARCHAR(45) NOT NULL,
+  `orderid` INT NOT NULL,
+  `amount` FLOAT NOT NULL,
+  `paymentmode` VARCHAR(50) NOT NULL,
+  `transactionid` INT NOT NULL,
+  PRIMARY KEY (`paymentid`),
+  INDEX `paymentsorders_idx` (`orderid` ASC) VISIBLE,
+  CONSTRAINT `paymentsorders`
+    FOREIGN KEY (`orderid`)
+    REFERENCES `ecommerce`.`orders` (`orderid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
